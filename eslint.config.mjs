2ea34f5709585eea4b1ab5cedc2f526d21d4ef3a
Alexsander <sparco@nxt.ru>
2024-04-15 12:08:50 +0500

    workspace dz done

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  {ignores: ["dist"]}
+];
+
